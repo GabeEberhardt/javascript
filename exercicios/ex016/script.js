abc60f0d2b622638631feb06dd326b4236c56c5f
Gabe <gabrielemilioeberhardt@gmail.com>
2026-08-14 21:52:26 -0300

    ex016 aprimorado

--- exercicios/ex016/script.js
+++ exercicios/ex016/script.js
@@ -3,16 +3,17 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
+    
    
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
-        img.scr = 'imagens/manha.png'
+        img.scr = 'imagens/manha1.png'
         document.body.style.background = '#e2cd9f'
     } else if (hora >= 12 && hora < 18) {
-        img.src = 'imagens/tarde.png'
+        img.src = 'imagens/tarde-1.png'
         document.body.style.background = '#b9846f'
     } else {
-        img.src = 'imagens/noite.png'
+        img.src = 'imagens/noite1.png'
         document.body.style.background = '#515154'
     }
 }
