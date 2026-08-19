d10000cf5a0733acf515aa3870a9cc7105fb980e
Gabe <gabrielemilioeberhardt@gmail.com>
2026-08-19 14:34:36 -0300

    ex020

---
+++ exercicios/ex020/script.js
@@ -0,0 +1,41 @@
+function contar() {
+    let inicioTxt = document.getElementById('txtinicio').value
+    let fimTxt = document.getElementById('txtfim').value
+    let passoTxt = document.getElementById('txtpasso').value
+    let resultado = document.getElementById('resultado')
+
+
+if (inicioTxt === '') {
+    window.alert('Preencha o campo Início para continuar.')
+    return
+}
+
+let inicio = parseInt(inicioTxt)
+let fim = parseInt(fimTxt)
+let passo = parseInt(passoTxt)
+
+if (inicio === fim) {
+    window.alert('Início e Fim não podem ser iguais.')
+    return
+}
+ 
+if (passo === 0 ) {
+    resultado.innerHTML = 'Passo não pode ser 0.'
+    return
+}
+
+passo = Math.abs(passo)
+
+resultado.innerHTML = 'Contagem: '
+
+if (inicio < fim) {
+    for (let c = inicio;c <= fim;c += passo) {
+        resultado.innerHTML += `${c} 👉`
+    }
+} else {
+    for (let c = inicio;c >= fim;c -= passo) {
+        resultado.innerHTML += `${c} 👉`
+    }
+}
+ resultado.innerHTML += '🏁'
+}
